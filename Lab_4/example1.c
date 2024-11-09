75331cefc0bc8500734450b2531b7f8c75df333a
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2024-11-10 02:57:15 +0500

    Added outputs for Lab 4 examples and programs

--- Lab_4/example1.c
+++ Lab_4/example1.c
@@ -5,7 +5,6 @@ int main(int argc, char **argv) {
     printf("I am before fork\n");
 
     fork();
-
     printf("I am after forking\n");
     printf("\t I am process having process id %d and parent process id is %d\n", getpid(), getppid());
 
