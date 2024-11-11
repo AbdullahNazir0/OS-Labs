ada4144718f6bf6ad826469709f34c70b817172e
AbdullahNazir0 <Abdullah.nazir289@gmail.com>
2024-11-11 20:42:51 +0500

    Added task1

--- Lab_4/task1.c
+++ Lab_4/task1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char **argv) {
 
@@ -9,6 +10,83 @@ int main(int argc, char **argv) {
     // child        child       child
     // child child
 
-    printf("I am before fork\n");
-    
+    // parent -> child1, child2, child3
+    // child1 -> child5, child6
+    // child5 -> child7, child8
+    // child2 -> child4
+
+    pid_t pid;
+    pid = fork();
+    if(pid < 0) {
+        printf("Fork failed\n");
+    } else if(pid == 0) {
+        // child 1
+        printf("Child 1\n");
+        pid = fork();
+        if(pid < 0) {
+            printf("Fork failed\n");
+        } else if(pid == 0) {
+            // child 5
+            printf("Child 5 (child of child 1)\n");
+            pid = fork();
+            if(pid < 0) {
+                printf("Fork failed\n");
+            } else if(pid == 0) {
+                // child 7
+                printf("Child 7 (child of child 5)\n");
+            } else {
+                // parent
+                pid = fork();
+                if(pid < 0) {
+                    printf("Fork failed\n");
+                } else if(pid == 0) {
+                    // child 8
+                    printf("Child 8 (child of child 5)\n");
+                } else {
+                    // parent
+                }
+            }
+        } else {
+            // parent
+            pid = fork();
+            if(pid < 0) {
+                printf("Fork failed\n");
+            } else if(pid == 0) {
+                // child 6
+                printf("Child 6 (child of child 1)\n");
+            } else {
+                // parent
+            }
+        }
+    } else {
+        // parent
+        pid = fork();
+        if(pid < 0) {
+            printf("Fork failed\n");
+        } else if(pid == 0) {
+            // child 2
+            printf("Child 2\n");
+            pid = fork();
+            if(pid < 0) {
+                printf("Fork failed\n");
+            } else if(pid == 0) {
+                // child 4
+                printf("Child 4 (child of child 2)\n");
+            } else {
+                // parent
+            }
+        } else {
+            // parent
+            pid = fork();
+            if(pid < 0) {
+                printf("Fork failed\n");
+            } else if(pid == 0) {
+                // child 3
+                printf("Child 3\n");
+            } else {
+                // parent
+            }
+        }
+    }
+
 }
